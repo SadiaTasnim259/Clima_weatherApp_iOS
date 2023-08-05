bd6512892a91447f5f5cfda6de2c3b7d707f8c8b
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-05 18:11:42 +0600

    IBoutlet and action connected

--- Clima_weatherApp/ViewController.swift
+++ Clima_weatherApp/ViewController.swift
@@ -8,11 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var weatherConditionView: UIImageView!
+    @IBOutlet weak var temparatureLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func locationButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+    }
+    
 
 
 }
